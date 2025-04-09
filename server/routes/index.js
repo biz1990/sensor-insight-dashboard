461b6c05b79d75104a8b88d66353fb38b7bb8562
biz1990 <phitruongtrolai@yahoo.com.vn>
2025-04-09 08:05:52 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,7 @@ const readingRoutes = require('./readingRoutes');
 const thresholdRoutes = require('./thresholdRoutes');
 const utilRoutes = require('./utilRoutes');
 const userRoutes = require('./userRoutes');
+const sensorReadingsRoutes = require('./sensorReadingsRoutes'); // Đường dẫn đến file sensorReadingsRoutes.js
 
 // Root API endpoint
 router.get('/', (req, res) => {
@@ -25,6 +26,8 @@ router.use('/locations', locationRoutes);
 router.use('/readings', readingRoutes);
 router.use('/thresholds', thresholdRoutes);
 router.use('/users', userRoutes);
+//app.use('/api', sensorReadingsRoutes); // Đặt tiền tố /api cho các routes
+router.use('/sensorreading',sensorReadingsRoutes);
 router.use('/', utilRoutes); // For utility routes like test-connection
 
 module.exports = router;
